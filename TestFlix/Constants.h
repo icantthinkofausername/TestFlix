c18d2172420d00729588bfef8fa20855284a6c73
Joshua Palermo <palermo@gmail.com>
2013-04-04 17:50:07 -0400

    modified the movie detail controller to resume operation after authentication

---
+++ TestFlix/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  TestFlix
+//
+//  Created by Joshua Palermo on 4/2/13.
+//  Copyright (c) 2013 Joshua Palermo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
---
+++ TestFlix/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  TestFlix
+//
+//  Created by Joshua Palermo on 4/2/13.
+//  Copyright (c) 2013 Joshua Palermo. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
